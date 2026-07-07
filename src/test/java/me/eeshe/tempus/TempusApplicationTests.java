d65d58f26c3d659994bd1fc5189306e19d4cbcbe
Eeshe <eeshedev@proton.me>
2026-07-07 17:40:59 -0400

    initial commit

---
+++ src/test/java/me/eeshe/tempus/TempusApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eeshe.tempus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
