5e5883b2bbc7ed7daa956e33133b114149b0a8f4
Eeshe <eeshedev@proton.me>
2026-09-16 15:54:27 -0400

    feat: Add angular controller

---
+++ src/main/java/me/eeshe/tempus/controller/AngularController.java
@@ -0,0 +1,13 @@
+package me.eeshe.tempus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AngularController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String forwardToIndex() {
+        return "forward:/";
+    }
+}
